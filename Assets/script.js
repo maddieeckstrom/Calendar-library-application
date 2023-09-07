8f28cef38350e89caea3683504b20a36d2054da0
Maddie <maddieeckstrom@gmail.com>
2023-09-07 08:44:04 -1000

    added event listener skeleton to console log when input on calendar is typed

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,26 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+
 $(function () {
+
+  var saveBtnEl = $('.saveBtn');
+  var hourNine = $('#hour-9');
+  var calInput = $('#textarea');
+
+  saveBtnEl.on('click', function() {
+    if (hourNine.value === '') {
+      console.log('Nothing here');
+    }
+    else {
+      console.log('Something here');
+    }
+  })
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
