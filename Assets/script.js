1be094ab677d2790e80fab793558d9a15078e0d0
Maddie <maddieeckstrom@gmail.com>
2023-09-07 12:35:10 -1000

    edited the click function to include jQuery this so that it each save button targets the data in that time box

--- Assets/script.js
+++ Assets/script.js
@@ -6,25 +6,39 @@
 
 $(function () {
 
-  var saveBtnEl = $('.saveBtn');
-  var hourNine = $('#hour-9');
-  var calInput = $('#textarea');
+  const saveBtnEl = $('.saveBtn');
 
   saveBtnEl.on('click', function() {
 
-    let input = calInput.val();
+    //this references the specific button we are clicking, now we can call jQuery methods on it
+    const $thisBtn = $(this);
+    console.log($thisBtn.attr('id'));
 
-    if (input !== '') {
-      console.log('Something here');
-      localStorage.setItem('hourNineItems', JSON.stringify(input));
-    }
-    else {
-      console.log('Nothing here');
-    }
+    const $relatedTextArea = $thisBtn.siblings("textarea");
+    console.log($relatedTextArea.val());
+
+    const $parentHour = $thisBtn.parent();
+    const hourId = $parentHour.attr('id');
+
+    console.log(hourId);
+
+    localStorage.setItem(hourId, $relatedTextArea.val());
+
   })
 
-  var hourNine = JSON.parse(localStorage.getItem('hourNineItems'));
+  const $timeBlocks = $('.time-block');
 
+  $timeBlocks.each(function() {
+    const $thisTimeBlock = $(this);
+
+    const $timeBlockText = localStorage.getItem($thisTimeBlock.attr('id'));
+
+    textarea.value = $timeBlockText;
+
+
+    //assign that getItem value to a variable, set it as a value of the text area of the time box
+
+  })
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -43,6 +57,4 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
